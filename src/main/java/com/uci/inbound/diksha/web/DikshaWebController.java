d30701fe591840a26a71722c149a13eb5d2f2574
radhay <radhay.anand@cred.club>
2023-03-29 19:52:50 +0530

    migrated to sp3 and graal

--- src/main/java/com/uci/inbound/diksha/web/DikshaWebController.java
+++ src/main/java/com/uci/inbound/diksha/web/DikshaWebController.java
@@ -12,14 +12,13 @@ import com.uci.utils.kafka.SimpleProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.bind.JAXBException;
+import jakarta.xml.bind.JAXBException;
 
 @Slf4j
 @RestController
