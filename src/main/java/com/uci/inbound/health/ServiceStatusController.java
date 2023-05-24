9547977f20eaa7c8118b69306dda0d3649740c2d
pankajjangid05 <pankaj.jangid@aurigait.com>
2023-05-24 12:47:02 +0530

    Removed static user segment code on prod

--- src/main/java/com/uci/inbound/health/ServiceStatusController.java
+++ src/main/java/com/uci/inbound/health/ServiceStatusController.java
@@ -130,47 +130,18 @@ public class ServiceStatusController {
     @RequestMapping(value = "/health/campaign", method = RequestMethod.GET, produces = { "application/json", "text/json" })
     public Mono<ResponseEntity<ApiResponse>> campaignUrlStatusCheck() {
 		return healthService.getCampaignUrlHealthNode().map(result ->
-			ApiResponse.builder().id("api.service.health.campaign")
-			.params(ApiResponseParams.builder().build())
-			.result(result)
-			.build())
-			.map(response -> {
-				if (((JsonNode)response.result).get("status").textValue().equals(Status.UP.getCode())) {
-					response.responseCode = HttpStatus.OK.name();
-					return new ResponseEntity<>(response, HttpStatus.OK);
-				}
-				else {
-					response.responseCode = HttpStatus.SERVICE_UNAVAILABLE.name();
-					return new ResponseEntity<>(response, HttpStatus.SERVICE_UNAVAILABLE);
-				}
-			});
-    }
-
-   	@RequestMapping(value = "/testUserSegment", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-	public ResponseEntity<JsonNode> testUserSegment(@RequestParam(name = "limit", required = false) String limit, @RequestParam(name = "offset", required = false) String offset) throws Exception {
-		log.info("Json : "+userSegmentJson1);
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode result = null;
-		if(userSegmentJson1 == null || userSegmentJson1.isEmpty()){
-			result = mapper.readTree("{\"error\" : \"User Segment JSON Not Found\"}");
-			return  ResponseEntity.ok(result);
-		} else{
-			result = mapper.readTree(userSegmentJson1);
-		}
-		return ResponseEntity.ok(result);
-	}
-
-	@RequestMapping(value = "/testUserSegment2", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-	public ResponseEntity<JsonNode> testUserSegment2(@RequestParam(name = "limit", required = false) String limit, @RequestParam(name = "offset", required = false) String offset) throws Exception {
-		log.info("Json : "+userSegmentJson2);
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode result = null;
-		if(userSegmentJson2 == null || userSegmentJson2.isEmpty()){
-			result = mapper.readTree("{\"error\" : \"User Segment JSON Not Found\"}");
-			return  ResponseEntity.ok(result);
-		} else{
-			result = mapper.readTree(userSegmentJson2);
-		}
-		return ResponseEntity.ok(result);
+						ApiResponse.builder().id("api.service.health.campaign")
+								.params(ApiResponseParams.builder().build())
+								.result(result)
+								.build())
+				.map(response -> {
+					if (((JsonNode) response.result).get("status").textValue().equals(Status.UP.getCode())) {
+						response.responseCode = HttpStatus.OK.name();
+						return new ResponseEntity<>(response, HttpStatus.OK);
+					} else {
+						response.responseCode = HttpStatus.SERVICE_UNAVAILABLE.name();
+						return new ResponseEntity<>(response, HttpStatus.SERVICE_UNAVAILABLE);
+					}
+				});
 	}
 }
