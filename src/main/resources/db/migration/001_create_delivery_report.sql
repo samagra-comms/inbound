ff9b212ff0225a485d1e53ec90eca29e86573efe
pankajjangid05 <pankaj.jangid@aurigait.com>
2023-08-22 12:57:06 +0530

    [Hot Fix] - Remove Partitioning from Table

--- src/main/resources/db/migration/001_create_delivery_report.sql
+++ src/main/resources/db/migration/001_create_delivery_report.sql
@@ -10,9 +10,7 @@ CREATE table IF NOT EXISTS delivery_report
     fcm_token     VARCHAR(200),
     message_state VARCHAR(10),
     created_on    TIMESTAMP NOT NULL DEFAULT NOW(),
-    PRIMARY KEY (created_on, id)
-) PARTITION BY RANGE (created_on);
+    PRIMARY KEY (id)
+);
 
 CREATE INDEX IF NOT EXISTS idx_bot_id ON delivery_report (bot_id);
-
-CREATE TABLE delivery_report_2023 PARTITION OF delivery_report FOR VALUES FROM ('2023-08-01') TO ('2024-07-31');
