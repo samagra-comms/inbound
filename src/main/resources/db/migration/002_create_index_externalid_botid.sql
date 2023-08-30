d87678cd13fe5fd61f9d89a1c3a52d75d0ebcc2c
pankajjangid05 <pankaj.jangid@aurigait.com>
2023-08-30 17:33:38 +0530

    [Hot Fix] - Added message_state in indexing delivery report

--- src/main/resources/db/migration/002_create_index_externalid_botid.sql
+++ src/main/resources/db/migration/002_create_index_externalid_botid.sql
@@ -1,1 +1,1 @@
-CREATE UNIQUE INDEX IF NOT EXISTS delivery_report_external_id_idx ON delivery_report (external_id,bot_id);
+CREATE UNIQUE INDEX IF NOT EXISTS delivery_report_external_id_message_state_idx ON public.delivery_report (external_id,bot_id,message_state);
